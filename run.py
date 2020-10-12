7ec6bbd0f7456b622050192603d340d823b026e0
Rydha Valenda <ryval.study@gmail.com>
2020-10-12 11:44:43 +0700

    get urls per page

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+# pakai session karena adanya login
 session = requests.Session()
 
 def login():
@@ -12,7 +13,6 @@ def login():
 
     res = session.post('http://0.0.0.0:9999/login', data=datas)
 
-
     soup = BeautifulSoup(res.text, 'html5lib')
 
     page_item = soup.find_all('li', attrs={'class': 'page-item'})
@@ -20,17 +20,30 @@ def login():
     return total_pages
 
 
-def get_urls():
-    print('getting urls.........')
+def get_urls(page):
+    print('getting urls.........  page()', format(page))
 
     params = {
-        'page': 1
+        'page': page
     }
     res = session.get('http://0.0.0.0:9999', params=params)
 
-    f = open('./res.html', 'w+')
-    f.write(res.text)
-    f.close()
+    # soup = BeautifulSoup(open('./res.html'), 'html5lib')
+
+    soup = BeautifulSoup(res.text, 'html5lib')
+
+    titles = soup.find_all('h4', attrs={'class': 'card-title'})
+    urls = []              # untuk dijadikan satu
+    for title in titles:
+        url = title.find('a')['href']
+        urls.append(url)
+
+    return urls
+
+    # sebagai result dari get_urls() page 1
+    # f = open('./res.html', 'w+')
+    # f.write(res.text)
+    # f.close()
 
 def get_detail():
     print('getting detail......')
@@ -39,8 +52,17 @@ def create_csv():
     print('csv generated...')
 
 def run():
-    login()
-    get_urls()
+    # login()
+    total_pages = login()
+    total_urls = []
+    for i in range(total_pages):    # in range biasa mulai dari 0 makanya di +1
+        page = i + 1
+        urls = get_urls(page)
+        total_urls += urls   # total_urls = total_urls + urls
+
+    print(total_urls)
+    print(len(total_urls))
+
     get_detail()
     create_csv()
 
